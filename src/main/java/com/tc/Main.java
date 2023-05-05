99348ce89a11b701a13ddd7dd38c3391ab04eabd
T-Rock99 <tcharles@appdev.yearup.org>
2023-05-05 09:42:37 -0400

    Third commit -- completed all menus outlined in subtask except for reports. Enhanced presentation of information with spacing that makes reading much cleaner.

--- src/main/java/com/tc/Main.java
+++ src/main/java/com/tc/Main.java
@@ -38,39 +38,44 @@ import java.time.format.DateTimeFormatter;
 public class Main {
     static ArrayList<Transaction> transactions = new ArrayList<>();
     static Scanner userInput = new Scanner (System.in);
+    static LocalDateTime now = LocalDateTime.now();
+    static DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    static DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm");
     public static void main(String[] args) {
         try{
-            FileReader transactions = new FileReader("./src/main/java/com/tc/transactions.csv");
-            BufferedReader bufferedReader = new BufferedReader(transactions);
-
             FileWriter transactionsUpdate = new FileWriter("./src/main/java/com/tc/transactions.csv", true);
             BufferedWriter bufferedWriter = new BufferedWriter(transactionsUpdate);
 
-            LocalDateTime now = LocalDateTime.now();
+            try{
+            System.out.println("Welcome to Ty's Ledger Application!\n"); // intro
+            Thread.sleep(1500);} catch (InterruptedException e){} // Gives user time to read intro screen
 
-            System.out.println("Welcome to Ty's Ledger Application!\n Taking you to the home screen...");
+            try{
+            System.out.println("Initializing...\n");
+            Thread.sleep(1500);} catch (InterruptedException e){}
 
             String input;
             do{
                 System.out.println("Please select an option:\n D) Add Deposit\n P) Make Payment\n L) Display Ledger Screen\n X) Exit Ty's Ledger App");
-
                 input = userInput.nextLine();
 
                 switch(input){
                     case "D":
-//                        transactionsUpdate.write(addDeposit());
-                        addDeposit();
+                        transactionsUpdate.write("\n" + addDeposit());
+                        System.out.println("\nDeposit has been successfully made!\n");
                         break;
                     case "P":
+                        transactionsUpdate.write("\n" + makePayment());
+                        System.out.println("\nPayment submitted successfully!\n");
                         break;
                     case "L":
+                        LedgerSubMenu();
                         break;
                     case "X":
+                        System.out.println("\nThank you for using Ty's Ledger Application! We look forward to servicing you again!"); // Exit message
                         break;
                     default:
-                        System.out.println("That's not a valid input -- Make sure your input is capitalized (Ex: \"L\" to display Ledger Screen");
-
-
+                        System.out.println("\nThat's not a valid input -- Make sure your input is capitalized (Ex: \"L\" to display the Ledger Screen.\n");
                 }
             } while (!input.equals("X"));
             transactionsUpdate.close();
@@ -84,9 +89,102 @@ public class Main {
 
     }
     public static String addDeposit(){
-        System.out.println("How much would you like to deposit?");
-        String deposit = userInput.nextLine();
+        System.out.println("\nWhat bank, company, or organization are you making a deposit with?");
+        String vendor = userInput.nextLine();
+        System.out.println("\nHow much would you like to deposit?");
+        float amount = userInput.nextFloat();
+        String formattedAmount = String.format("$%,.2f", amount);
+        String date = now.format(dateFormat);
+        String time = now.format(timeFormat);
+        String deposit = date + "|" + time + "|Deposit|" + vendor + "|" + formattedAmount;
         return deposit;
+    }
+    public static String makePayment(){
+        System.out.println("\nWhat bank, company, or organization are you making a payment to?");
+        String vendor = userInput.nextLine();
+        System.out.println("\nPlease enter the amount of this payment:");
+        float amount = userInput.nextFloat();
+        String formattedAmount = String.format("-$%,.2f", amount);
+        String date = now.format(dateFormat);
+        String time = now.format(timeFormat);
+        String payment = date + "|" + time + "|Payment|" + vendor + "|" + formattedAmount;
+        return payment;
+    }
+    public static void LedgerSubMenu() throws IOException {
+        String input;
+        do{
+            input = userInput.nextLine();
+            System.out.println("\nWelcome to the Ledger Screen. Please select one of the following options:\n A) Display all entries\n D) Display all deposits\n P) Display all payments\n R) Run a pre-defined or custom report\n B) Go back to the home screen");
+            switch(input){
+                case "A":
+                    FileReader transactions1 = new FileReader("./src/main/java/com/tc/transactions.csv");
+                    BufferedReader bufferedReader1 = new BufferedReader(transactions1);
+                    String allEntries;
+                    while((allEntries = bufferedReader1.readLine()) != null){
+                    System.out.println("\n" + allEntries);}
+                    break;
+                case "D":
+                    FileReader transactions2 = new FileReader("./src/main/java/com/tc/transactions.csv");
+                    BufferedReader bufferedReader2 = new BufferedReader(transactions2);
+                    String allDeposits;
+                    while((allDeposits = bufferedReader2.readLine()) != null){
+                        if(allDeposits.contains("Deposit")){
+                            System.out.println("\n" + allDeposits);
+                        }
+                    }
+                    break;
+                case "P":
+                    FileReader transactions3 = new FileReader("./src/main/java/com/tc/transactions.csv");
+                    BufferedReader bufferedReader3 = new BufferedReader(transactions3);
+                    String allPayments;
+                    while((allPayments = bufferedReader3.readLine()) != null){
+                        if(allPayments.contains("Payment")){
+                            System.out.println("\n" + allPayments);
+                        }
+                    }
+                    break;
+                case "R":
+                    reportsSubMenu();
+                    break;
+                case "B":
+                    System.out.println("\nSending you back to the home screen...\n"); // Submenu exit message
+                    try{Thread.sleep(750);}catch(InterruptedException e){}
+                    break;
+                default:
+                    System.out.println("\nThat's not a valid input -- Make sure your input is capitalized (Ex: \"P\" to display all payments.\n");
+            }
+        }while(!input.equals("B"));
+    }
+    public static void reportsSubMenu() {
+        String input;
+        do {
+            input = userInput.nextLine();
+            System.out.println("\nWhat kind of report would you like to run?\n 1) Month to Date\n 2) Previous Month\n 3) Year to Date\n 4) Previous Year\n 5) Search by Vendor\n 0) Back to home page");
+            switch (input) {
+                case "1":
+                    BufferedReader bufferedReader = new BufferedReader(new FileReader("./src/main/java/com/tc/transactions.csv"));
+                    int currentYear = now.getYear();
+                    String formattedYear = String.format("$04d", currentYear);
+                    int currentMonth = now.getMonthValue();
+                    String formattedMonth = String.format("$02d", now.getMonthValue());
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "0":
+                    System.out.println("\nSending you back to the home screen...\n"); // Submenu exit message
+                    try {
+                        Thread.sleep(750);
+                    } catch (InterruptedException e) {
+                    }
+                    break;
+            }
+        } while (!input.equals("0"));
     }
 
 }
