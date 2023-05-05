25df6afc3804a4ebf110cef228c04cbbe8426bd6
T-Rock99 <tcharles@appdev.yearup.org>
2023-05-05 03:12:22 -0400

    Second commit -- began work on home menu, partially completed add deposit screen.

--- src/main/java/com/tc/Main.java
+++ src/main/java/com/tc/Main.java
@@ -1,11 +1,14 @@
 package com.tc;
+
 import java.util.*;
 import java.util.regex.*;
 import java.io.*;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 // Application intent: Track all financial transactions for general variety of purposes.
-// Note: All transactions should read from and save to the "inventory.csv" file, in the following format:
-// date|time|description|vendor|amount (more examples in "invetory.csv" by default.
+// Note: All transactions should read from and save to the "transactions.csv" file, in the following format:
+// date|time|description|vendor|amount
 
 // Home Screen:
 //  - D) Add Deposit - prompt user for information and save it to csv file
@@ -33,7 +36,57 @@ import java.io.*;
 //  - X) Exit - exit the application
 
 public class Main {
+    static ArrayList<Transaction> transactions = new ArrayList<>();
+    static Scanner userInput = new Scanner (System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try{
+            FileReader transactions = new FileReader("./src/main/java/com/tc/transactions.csv");
+            BufferedReader bufferedReader = new BufferedReader(transactions);
+
+            FileWriter transactionsUpdate = new FileWriter("./src/main/java/com/tc/transactions.csv", true);
+            BufferedWriter bufferedWriter = new BufferedWriter(transactionsUpdate);
+
+            LocalDateTime now = LocalDateTime.now();
+
+            System.out.println("Welcome to Ty's Ledger Application!\n Taking you to the home screen...");
+
+            String input;
+            do{
+                System.out.println("Please select an option:\n D) Add Deposit\n P) Make Payment\n L) Display Ledger Screen\n X) Exit Ty's Ledger App");
+
+                input = userInput.nextLine();
+
+                switch(input){
+                    case "D":
+//                        transactionsUpdate.write(addDeposit());
+                        addDeposit();
+                        break;
+                    case "P":
+                        break;
+                    case "L":
+                        break;
+                    case "X":
+                        break;
+                    default:
+                        System.out.println("That's not a valid input -- Make sure your input is capitalized (Ex: \"L\" to display Ledger Screen");
+
+
+                }
+            } while (!input.equals("X"));
+            transactionsUpdate.close();
+
+
+        } catch (IOException e){
+            e.printStackTrace();
+            System.out.println("There is a \"catch\" error! This error message is for developer use.");
+        }
+
+
     }
+    public static String addDeposit(){
+        System.out.println("How much would you like to deposit?");
+        String deposit = userInput.nextLine();
+        return deposit;
+    }
+
 }
